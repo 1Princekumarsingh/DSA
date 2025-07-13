1e7fcd65424020a350189d7076af253ddd7d3930
Prince Kumar Singh <kprinces888@gmail.com>
2025-07-13 21:53:32 +0530

    Bs-N Room Problem

---
+++ src/Binary_Search/N_meetings_in_one_room_gfg.java
@@ -0,0 +1,27 @@
+package Binary_Search;
+import java.util.*;
+public class N_meetings_in_one_room_gfg {
+
+    public int maxMeetings(int start[], int end[]) {
+        int n = start.length;
+
+        ArrayList<Integer> idx  = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            idx.add(i);
+        }
+        Collections.sort(idx,(a,b)->Integer.compare(end[a],end[b]));
+
+        int totalMeetings = 1;
+        int lastEndTime = end[idx.get(0)];
+
+        for(int i=1; i<n; i++){
+            int x = idx.get(i);
+            if(start[x]>lastEndTime){
+                totalMeetings++;
+                lastEndTime = end[x];
+            }
+        }
+        return totalMeetings;
+    }
+}
+
